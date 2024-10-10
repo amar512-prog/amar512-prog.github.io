f666814336828d6972ad8c24b317c1667171c087
amar512-prog <70102514+amar512-prog@users.noreply.github.com>
2024-10-10 16:30:39 +0530

    Update weather.js

--- src/js/weather.js
+++ src/js/weather.js
@@ -1,3 +1,4 @@
+document.addEventListener("DOMContentLoaded", function () {
 // Make an HTTP GET request to the OpenWeatherMap API
 fetch('https://api.openweathermap.org/data/2.5/weather?q=New%20Delhi&appid=565f792c8c70e572d7f3fa7d03ff7c57')
   .then(response => response.json())
@@ -11,7 +12,31 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q=New%20Delhi&appid=565f7
     console.log(`Today's weather in New Delhi, India: ${weather.description}`);
     console.log(`Temperature: ${temperature} K`);
     console.log(`Humidity: ${humidity}%`);
+    // Update the weather information on the webpage
+    document.getElementById('temp-value').textContent = temperature;
+    document.getElementById('humidity-value').textContent = humidity;
+    document.getElementById('weather-desc-value').textContent = weather.description;
+
+    // Set the appropriate weather icon based on the weather condition
+    const weatherIcon = document.getElementById('weather-description').querySelector('i');
+    switch (weather.main) {
+      case 'Clear':
+        weatherIcon.classList.add('fas', 'fa-sun');
+        break;
+      case 'Clouds':
+        weatherIcon.classList.add('fas', 'fa-cloud');
+        break;
+      case 'Rain':
+        weatherIcon.classList.add('fas', 'fa-cloud-rain');
+        break;
+      case 'Snow':
+        weatherIcon.classList.add('fas', 'fa-snowflake');
+        break;
+      default:
+        weatherIcon.classList.add('fas', 'fa-cloud');
+    }
   })
   .catch(error => {
     console.error('Error:', error);
   });
+});
