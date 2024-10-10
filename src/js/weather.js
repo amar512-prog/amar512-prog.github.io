13a756f73db73ab3a4114875cabd31296fadaaba
amar512-prog <70102514+amar512-prog@users.noreply.github.com>
2024-10-10 16:07:25 +0530

    Update weather.js

--- src/js/weather.js
+++ src/js/weather.js
@@ -1,5 +1,5 @@
 // Make an HTTP GET request to the OpenWeatherMap API
-fetch('https://api.openweathermap.org/data/2.5/weather?q=New%20Delhi&appid=YOUR_API_KEY')
+fetch('https://api.openweathermap.org/data/2.5/weather?q=New%20Delhi&appid=edf50ea1e4d24be7de889fbbff1b7134')
   .then(response => response.json())
   .then(data => {
     // Extract the weather information from the response
