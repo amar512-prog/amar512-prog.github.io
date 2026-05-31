b00300f577fa30345d62270fd13e059c47be57fb
Amar Agnihotri <amaragnihotri1@gmail.com>
2026-05-31 12:24:46 +0530

    Hide support section for now

--- src/js/page-support.js
+++ src/js/page-support.js
@@ -1,28 +1,12 @@
 (function () {
   const mount = document.querySelector("[data-page-support]");
+  const section = mount?.closest(".page-support-section");
 
   if (!mount) {
     return;
   }
 
-  mount.innerHTML = `
-    <div class="page-support__copy">
-      <p class="eyebrow">Support</p>
-      <h2>Keep the next write-up moving</h2>
-      <p class="page-support__intro">
-        If a note or paper review helped you think more clearly, and you would like to
-        support more work like this, the easiest place to start is GitHub.
-      </p>
-    </div>
-
-    <div class="page-support__actions">
-      <a class="button" href="https://github.com/amar512-prog">Support on GitHub</a>
-      <a
-        class="button button-secondary"
-        href="https://github.com/amar512-prog/amar512-prog.github.io"
-      >
-        View the repository
-      </a>
-    </div>
-  `;
+  if (section) {
+    section.hidden = true;
+  }
 })();
